80ea001a82335fa5de349f7b4eacea9c16d74812
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 19:14:29 +0530

    Update Date_Time_HandsOn.py

--- Date_Time_HandsOn.py
+++ Date_Time_HandsOn.py
@@ -1,142 +1,119 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
+#importing libraries
 
-
-import pandas as pd   #importing libraries
+import pandas as pd   
 
 
-# In[2]:
+#creating lambda function for accessing the data in date time format.
 
+d_parser = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %I-%p') 
 
-d_parser = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %I-%p')      #creating lambda function for accessing the data in date time format.
-df = pd.read_csv('ETH_1h.csv', parse_dates=['Date'], date_parser=d_parser)  #parse_date will convert the date object into date-time data type.
+#parse_date will convert the date object into date-time data type.
 
-
-# In[3]:
+df = pd.read_csv('ETH_1h.csv', parse_dates=['Date'], date_parser=d_parser)  
 
 
 df.head()
 
+#day_name will fetch the day for perticular date.
 
-# In[4]:
+df.loc[0, 'Date'].day_name()     
 
+#fetched the day for whole df
+  
+df['Date'].dt.day_name()         
 
-df.loc[0, 'Date'].day_name()     #day_name will fetch the day for perticular date.
+ #added it to the df.
 
-
-# In[5]:
-
-
-df['Date'].dt.day_name()          #fetched the day for whole df
-
-
-# In[6]:
-
-
-df['DayOfWeek'] = df['Date'].dt.day_name()   #added it to the df.
-
-
-# In[7]:
-
+df['DayOfWeek'] = df['Date'].dt.day_name()  
 
 df
 
 
-# In[8]:
+ #Earliest date in df
 
 
-df['Date'].min()               #Earliest date in df
+df['Date'].min()              
 
 
-# In[9]:
+#Most recent date in df.
 
 
-df['Date'].max()               #Most recent date in df.
+df['Date'].max()               
 
 
-# In[10]:
+#span between two dates.
 
 
-df['Date'].max() - df['Date'].min()     #span between two dates.
+df['Date'].max() - df['Date'].min()     
 
 
-# In[11]:
+#fetching details betwwne dates.
 
 
-filt = (df['Date'] >= pd.to_datetime('2019-01-01')) & (df['Date'] < pd.to_datetime('2020-01-01'))  #fetching details betwwne dates.
+filt = (df['Date'] >= pd.to_datetime('2019-01-01')) & (df['Date'] < pd.to_datetime('2020-01-01'))  
 df.loc[filt]    
 
 
-# In[12]:
+ #setting index as date. This will make slicing easy.
 
 
-df.set_index('Date', inplace=True)  #setting index as date. This will make slicing easy.
+df.set_index('Date', inplace=True) 
 
 
-# In[13]:
 
 
 df
 
 
-# In[14]:
+
 
 
 df['2019']
 
-
-# In[15]:
-
-
 df['2020-01':'2020-02']
 
 
-# In[16]:
+#getting mean for close column from jan to feb. 
+
+df['2020-01':'2020-02']['Close'].mean()  
 
 
-df['2020-01':'2020-02']['Close'].mean()  #getting mean for close column from jan to feb. 
+#max high value in jan month
+df['2020-01-01']['High'].max()    
 
 
-# In[17]:
 
-
-df['2020-01-01']['High'].max()    #max high value in jan month
-
-
-# In[18]:
 
 
 highs = df['High'].resample('D').max()    #resample is used fetch data. Like we can fetch data Day wise,week wise.
 highs['2020-01-01']
 
 
-# In[19]:
+
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[20]:
+
+#ploting the data. X-axis will be always time.
+
+highs.plot()             
 
 
-highs.plot()             #ploting the data. X-axis will be always time.
 
-
-# In[21]:
 
 
 df.resample('W').mean()
 
 
-# In[22]:
+
 
 
 df.resample('W').agg({'Close': 'mean', 'High': 'max', 'Low': 'min', 'Volume': 'sum'})
 
 
-# In[ ]:
 
 
 
