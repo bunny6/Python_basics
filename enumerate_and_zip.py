29385a1f29fcd409d68a5ccc15368e1bde093a4f
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 19:22:47 +0530

    Update enumerate_and_zip.py

--- enumerate_and_zip.py
+++ enumerate_and_zip.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[4]:
 
 
 for i in range(0,11):
     print(i)
 
 
-# In[5]:
 
 
 word="hello"
@@ -16,7 +12,7 @@ for i in enumerate(word):                  #enumerate will return the index with
     print(i)
 
 
-# In[6]:
+
 
 
 mylist1=[1,2,3]
@@ -24,7 +20,6 @@ mylist2=['a','b','c']
 list(zip(mylist1,mylist2))                             #zipping two list
 
 
-# In[ ]:
 
 
 
