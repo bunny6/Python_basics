ad42ca47d6ea03631905196c918a2522167a24ce
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-15 19:17:05 +0530

    Update Generator.py

--- Generator.py
+++ Generator.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
 
 def topten():
     yield 1
@@ -14,7 +9,7 @@ print(values.__next__())
 print(values.__next__())
 
 
-# In[4]:
+
 
 
 def topten():
@@ -30,7 +25,6 @@ for i in values:
     print(i)
 
 
-# In[ ]:
 
 
 
