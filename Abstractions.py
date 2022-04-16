5af687fb9f4fa468ca5a333ba5704c991efbd16e
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-16 16:07:20 +0530

    Update Abstractions.py

--- Abstractions.py
+++ Abstractions.py
@@ -1,12 +1,7 @@
 #import ABC,abstractmethod from abc.
-
-
 from abc import ABC,abstractmethod     
 
 #abstraction means hiding the implementation part and showing the declaration part.
-
-
-
 class ss(ABC):                           
     @abstractmethod
     def display(self):
