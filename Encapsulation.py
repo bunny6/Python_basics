6d48df28d50140c4cc9769636d9be671778dec45
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-16 16:13:14 +0530

    Update Encapsulation.py

--- Encapsulation.py
+++ Encapsulation.py
@@ -1,17 +1,10 @@
-
-
-
 class Encap:                  #Public is an access spicifier. By these spicifiers only we can acheive encapsulation.
     def fan(self):            #public is the bydefault access specifier.
         print("hello")
 c1=Encap()
 c1.fan()
 
-
 #Private
-
-
-
 
 class ac:                        #private is an access specifier. The methods and functions can be used by this class only.
     __a=10;
