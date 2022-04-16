d881eef6d55d894d9ab87f6d6203a8dee7541ae3
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-16 16:10:20 +0530

    Update Data_Types.py

--- Data_Types.py
+++ Data_Types.py
@@ -1,194 +1,77 @@
 #strings.
-
-
 my_string='Hello world'    #declaring a string
-
-
-
-
 
 my_string[0]            #fetch the string words.
 
-
-
-
-
 my='abcdefghij'
 
-
-
- 
 my[2:]             #this will print:cdefghij
-
-
-
 
 my[1:3]             #this will print:bc
 
-
-
-
-
 my[::-1]           #wil reverse the string.
-
-
-
-
 
 my_string.upper()   #will do upper case all the characters.
 
-
-
-
 print("this is a {} {}".format('good','string'))    #.format technique.
-
-
-
-
-
-
 
 li=[1,2,3]
 
-
-
-
-
 li[0]='New value'
 
-
-
-
-
 li
-
-
-
 
 li[1:]
 
-
-
 li.append('Six')     #append is use to add something in list.
 
-
-
-
-
 li
-
-
-
-
 
 li.pop(1)     #pop will remove the last value from the list.
 
-
-
-
-
 li
-
 
 #Dictionary
 
-
-
-
 d={'key1':'Value1','Key2':'value2'}
-
 
 d['key1']
 
-
-
-
 d['key3']='value3'
-
-
-
-
 
 d
 
-
-# In[28]:
-
-
 d['key3']
-
-
-# In[29]:
-
 
 d1={'Shubham':45,'Dileep':55}
 
-
 d1.keys()
-
 
 d1.items()
 
-
-
 d1.values()
-
 
 #Tuples
 
-
-
-
 tu=(1,2,3)
-
-
-
-
 
 tu[0]
 
-
-
-
-
 tup=('this','is','tuple')
-
-
-
 
 li=['this','is','list']
 
-
-
-
-
 type(tup)
-
-
-
-
 
 type(li)
 
-
 #Sets
-
 
 myset=set('aa')
 
-
-
-
 myset
 
-
-
-
 myset.add('a')
-
-
-
-
 
 myset
 
