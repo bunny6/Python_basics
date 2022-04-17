2ca0f9d72905240cbc9ad1fbf39bb0a94f55e9a4
bunny6 <43778618+bunny6@users.noreply.github.com>
2022-04-18 01:24:32 +0530

    Update enumerate_and_zip.py

--- enumerate_and_zip.py
+++ enumerate_and_zip.py
@@ -1,19 +1,10 @@
 
-
-
 for i in range(0,11):
     print(i)
-
-
-
 
 word="hello"
 for i in enumerate(word):                  #enumerate will return the index with the value.
     print(i)
-
-
-
-
 
 mylist1=[1,2,3]
 mylist2=['a','b','c']
